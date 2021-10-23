13a0396c58fcadbbf2c6cf8f8e85690ff1d0a9cc
OwlinOhana <karamelsweeti363@gmail.com>
2021-10-23 17:00:24 +0700

    update

--- lib/say_some_user.rb
+++ lib/say_some_user.rb
@@ -1,11 +1,12 @@
 class SaySomething
 
-  def initialize(temper)
+  def initialize(temper, im_temp)
     @temper = temper
+    @im_temp = im_temp
   end
 
   def res_comp
-    @temper = ComputTemp.comput
+    @temper = ComputTemp.new(@temper, @im_temp).comput
     if @temper == 'error'
       puts 'Вы допустили ошибку, попытайтесь снова'
     else
