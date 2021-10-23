f5fd61c8f4fafa68f30bc3ed7570b0686f586eaf
OwlinOhana <karamelsweeti363@gmail.com>
2021-10-23 17:23:14 +0700

    update

--- lib/comput_temp.rb
+++ lib/comput_temp.rb
@@ -7,20 +7,20 @@ class ComputTemp
   def comput
     temperature = ModiTemp.new(@temper)
     case @im_temp
-      when 'CK'
-        @temper = temperature.comput_ck
-      when 'KC'
-        @temper = temperature.comput_kc
-      when 'CF'
-        @temper = temperature.comput_cf
-      when 'FC'
-        @temper = temperature.comput_fc
-      when 'FK'
-        @temper = temperature.comput_fk
-      when 'KF'
-        @temper = temperature.comput_kf
-      else
-        @temper = 'error'
+    when 'CK'
+      @temper = temperature.comput_ck
+    when 'KC'
+      @temper = temperature.comput_kc
+    when 'CF'
+      @temper = temperature.comput_cf
+    when 'FC'
+      @temper = temperature.comput_fc
+    when 'FK'
+      @temper = temperature.comput_fk
+    when 'KF'
+      @temper = temperature.comput_kf
+    else
+      @temper = 'error'
     end
   end     
 end
