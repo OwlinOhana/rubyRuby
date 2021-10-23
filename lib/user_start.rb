62b0c7f6f036dad9441d10aff0e9da63fda8f9dd
OwlinOhana <karamelsweeti363@gmail.com>
2021-10-23 17:16:07 +0700

    update

--- lib/user_start.rb
+++ lib/user_start.rb
@@ -1,14 +1,16 @@
 class UserStart
   @om_temp = 'a'
 
-  def arg
+  def arg_temp_date_num
     puts 'Введите значение'
     @temper = gets.chomp.to_f
+  end 
+
+  def arg_temp_date_val
     puts 'Введите единицу измерения'
     @im_temp = gets.chomp
     puts 'Введите единицу измерения,в которую необходимо перевести'
     @om_temp = gets.chomp
-    @im_temp += @om_temp    
-    return @temper, @im_temp
-  end 
+    @im_temp += @om_temp
+  end
 end
