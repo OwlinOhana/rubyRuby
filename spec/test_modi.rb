b8b32b890d5945de02fed37f1f0fc2b883d0f7a4
OwlinOhana <karamelsweeti363@gmail.com>
2021-10-23 15:06:39 +0700

    update

---
+++ spec/test_modi.rb
@@ -0,0 +1,51 @@
+require './lib/modi_temp'
+RSpec.describe ModiTemp do
+
+  describe '#correct all, CF' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_cf
+    end
+    it { is_expected.to eq 1832.0 }
+  end
+
+  describe '#correct all, FC' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_fc
+    end
+    it { is_expected.to eq 537.78 }
+  end
+
+  describe '#correct all, FK' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_fk
+    end
+    it { is_expected.to eq 810.93 }
+  end
+  
+  describe '#correct all, KF' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_kf
+    end
+    it { is_expected.to eq 1340.33 }
+  end
+
+  describe '#correct all, KC' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_kc
+    end
+    it { is_expected.to eq 726.85 }
+  end
+
+  describe '#correct all, CK' do
+    subject do
+      temperature = ModiTemp.new(1000)
+      temperature.comput_ck
+    end
+    it { is_expected.to eq 1273.15 }
+  end
+end
