e45519523e621c82de8747c9042f92b7282d329b
OwlinOhana <karamelsweeti363@gmail.com>
2021-11-17 15:04:00 +0700

    new version

--- spec/comput_temper_spec.rb
+++ spec/comput_temper_spec.rb
@@ -1,34 +1,34 @@
 require './lib/comput_temp'
 RSpec.context ComputTemper do
   describe '#converter' do
-  context 'F->C' do
-    subject { ComputTemper.new(1000, 'C', 'F').comput_temper }
-    it { is_expected.to eq 1832.0 }
-  end
+    context 'C->F' do
+      subject { ComputTemper.new(1000, 'C', 'F').comput_temper }
+      it { is_expected.to eq 1832.0 }
+    end
 
-  context 'F->C' do
-    subject { ComputTemper.new(1000, 'F', 'C').comput_temper }
-    it { is_expected.to eq 537.77 }
-  end
+    context 'F->C' do
+      subject { ComputTemper.new(1000, 'F', 'C').comput_temper }
+      it { is_expected.to eq 537.77 }
+    end
 
-  context 'F->K' do
-    subject { ComputTemper.new(1000, 'F', 'K').comput_temper }
-    it { is_expected.to eq 810.92 }
-  end
+    context 'F->K' do
+      subject { ComputTemper.new(1000, 'F', 'K').comput_temper }
+      it { is_expected.to eq 810.92 }
+    end
 
-  context 'K->F' do
-    subject { ComputTemper.new(1000, 'K', 'F').comput_temper }
-    it { is_expected.to eq 1340.6 }
-  end
+    context 'K->F' do
+      subject { ComputTemper.new(1000, 'K', 'F').comput_temper }
+      it { is_expected.to eq 1340.6 }
+    end
 
-  context 'K->C' do
-    subject { ComputTemper.new(1000, 'K', 'C').comput_temper }
-    it { is_expected.to eq 726.85 }
-  end
+    context 'K->C' do
+      subject { ComputTemper.new(1000, 'K', 'C').comput_temper }
+      it { is_expected.to eq 726.85 }
+    end
 
-  context 'C->K' do
-    subject { ComputTemper.new(1000, 'C', 'K').comput_temper }
-    it { is_expected.to eq 1273.15 }
-  end
+    context 'C->K' do
+      subject { ComputTemper.new(1000, 'C', 'K').comput_temper }
+      it { is_expected.to eq 1273.15 }
+    end
   end
 end
